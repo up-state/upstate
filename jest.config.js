e77daa492932d0e8f75cf6a8fe1af2fe444a9762
Dennis Breitling <breitling@posteo.de>
2021-04-30 16:26:42 +0200

    Adds client unit tests with Jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  verbose: true,
+  moduleFileExtensions: ["js", "ts", "json", "vue"],
+  transform: {
+    "^.+\\.ts$": "ts-jest",
+    "^.+\\.vue$": "vue-jest",
+  },
+  moduleNameMapper: {
+    "\\.(css|scss|svg|png|jpg)$": "identity-obj-proxy",
+  },
+};
---
+++ src/client/components/another/Another.test.ts
@@ -0,0 +1,10 @@
+import { mount } from "@vue/test-utils";
+import Another from "./Another.vue";
+
+describe("Another.test.ts", () => {
+  it("renders message", () => {
+    const wrapper = mount(Another);
+
+    expect(wrapper.text()).toContain("Another");
+  });
+});
