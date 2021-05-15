1aeb18f6c0b02a030ea056b61a00c2d50fbcefae
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2021-05-15 14:33:31 +0200

    use proper logger for db errors

--- src/server/app.controller.ts
+++ src/server/app.controller.ts
@@ -1,25 +1,22 @@
-import {Controller, Get} from '@nestjs/common'
+import { Controller, Get } from '@nestjs/common'
 import * as db from 'zapatos/db'
-import {ServerTimeResponse} from '../shared/models'
-import {AppService} from './app.service'
-import {DBPool} from './database/db-pool/db-pool.service'
-
+import { ServerTimeResponse } from '../shared/models'
+import { AppService } from './app.service'
+import { DBPool } from './database/db-pool/db-pool.service'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService,
-              private readonly pool: DBPool) {
-  }
+  constructor(private readonly appService: AppService, private readonly pool: DBPool) {}
 
   @Get('/users')
   users() {
-    return db.transaction(this.pool, db.IsolationLevel.Serializable, async tx => {
+    return db.transaction(this.pool, db.IsolationLevel.Serializable, async (tx) => {
       return db.select('user', db.all).run(tx)
-    });
+    })
   }
 
   @Get('/api/server-time')
   getServerTime(): ServerTimeResponse {
-    return {servertime: new Date().toLocaleString()}
+    return { servertime: new Date().toLocaleString() }
   }
 }
--- src/server/database/db-pool/db-pool.service.ts
+++ src/server/database/db-pool/db-pool.service.ts
@@ -1,16 +1,17 @@
-import { Injectable, OnModuleDestroy } from '@nestjs/common'
+import { Injectable, Logger, OnModuleDestroy } from '@nestjs/common'
 import { Pool } from 'pg'
 
 @Injectable()
 export class DBPool extends Pool implements OnModuleDestroy {
+  private readonly logger = new Logger(DBPool.name)
+
   constructor() {
     // TODO: inject or use some sort of configuration service
-    super({connectionString: 'postgresql://postgres:postgres@localhost:5432/postgres'})
-    // TODO: Use a proper logger
-    this.on('error', err => console.error(err))
+    super({ connectionString: 'postgresql://postgres:postgres@localhost:5432/postgres' })
+    this.on('error', (err) => this.logger.error(err))
   }
 
-  onModuleDestroy(): Promise<void>  {
+  onModuleDestroy(): Promise<void> {
     return this.end()
   }
 }
