bdd78081d1f622379d128d8e2765d374519fe13a
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2021-05-15 15:34:10 +0200

    use logger for startup/shutdown logging

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,21 +1,22 @@
-import { INestApplication } from '@nestjs/common'
+import { INestApplication, Logger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { Config } from './config/config.service'
 
 let app: INestApplication
+const logger = new Logger('main')
 
 async function bootstrap() {
   app = await NestFactory.create(AppModule)
   const config = app.get(Config)
   await app.listen(config.port, '0.0.0.0')
-  console.log(`upstate server running @ ${await app.getUrl()}`)
+  logger.log(`upstate server running @ ${await app.getUrl()}`)
 }
 
 bootstrap()
 
 async function closeGracefully(signal: NodeJS.Signals) {
-  console.log(`*^!@4=> Received signal to terminate: ${signal}`)
+  logger.log(`*^!@4=> Received signal to terminate: ${signal}`)
 
   await app.close()
   process.exit()
